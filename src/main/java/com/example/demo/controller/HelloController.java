fdd04768a6f012ffa4269d5ca6edce317093c589
Jonathan Faber <jonathan.faber@autentia.com>
2020-10-23 09:46:21 +0200

    structure(controller, service, repository)

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,11 +1,10 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
 
     @GetMapping("/hello")
     public String getHello() {
