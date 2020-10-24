1cf00d99964e2b9b3201dde72ca3ba57baff4347
Jonathan Faber <jonathan.faber@autentia.com>
2020-10-24 12:21:44 +0200

    UserEndpointIT created

--- src/test/java/com/example/demo/endpoints/UserEndpointIT.java
+++ src/test/java/com/example/demo/endpoints/UserEndpointIT.java
@@ -1,21 +1,36 @@
-package com.example.demo;
+package com.example.demo.endpoints;
 
+import com.example.demo.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class DemoIT {
+public class UserEndpointIT {
     @Autowired
     private TestRestTemplate restTemplate;
 
     @LocalServerPort
     private int port;
+
+    @Test
+    public void user_endpoint_should_return_all_users() {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity entity = new HttpEntity(headers);
+
+        ResponseEntity<List<User>> response = this.restTemplate.exchange(createUrlWith("/users"), HttpMethod.GET, entity, new ParameterizedTypeReference<List<User>>() {
+        });
+
+        assertEquals(2, response.getBody().size());
+    }
 
     @Test
     public void hello_endpoint_should_return_hello_world() {
